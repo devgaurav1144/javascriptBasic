138b59e86615c5829de32342f3bbf58197c2a5d1
Gaurav Singh <devsinghgaurav.1144@gmail.com>
2025-07-28 15:52:28 +0530

    Adding Filtter functionality

---
+++ filtter.js
@@ -0,0 +1,63 @@
+// const num = [1,2,3,4,5,6,7,8,9];
+
+// let result = num.filter((numm)=> {
+// return numm > 4
+// });
+// console.log(result);
+
+// let numlist=[];
+// let result = num.filter((numm)=> 
+//  numm > 4
+// );
+// console.log(result);
+
+
+// num.forEach((res)=>{
+//    if(res>4){
+//     numlist.push(res)
+//    }
+// })
+
+// console.log(numlist);
+
+
+
+
+
+
+const listColors = [
+	{
+		color: "red",
+		value: "#f00"
+	},
+	{
+		color: "green",
+		value: "#0f0"
+	},
+	{
+		color: "blue",
+		value: "#00f"
+	},
+	{
+		color: "cyan",
+		value: "#0ff"
+	},
+	{
+		color: "magenta",
+		value: "#f0f"
+	},
+	{
+		color: "yellow",
+		value: "#ff0"
+	},
+	{
+		color: "black",
+		value: "#000"
+	}
+];
+
+let result = listColors.filter((cl)=>cl.color="Yellow");
+
+console.log(result);
+
+
