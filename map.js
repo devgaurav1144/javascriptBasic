5336f2b29a70b20828e421e21f5daea109edba5c
Gaurav Singh <devsinghgaurav.1144@gmail.com>
2025-07-28 16:34:09 +0530

    update Map.js file

--- map.js
+++ map.js
@@ -10,3 +10,10 @@ for (const [key,value] of map) {
 
 
 // Note: for..of loop not work for object
+
+
+const num = [1,2,3,4,5,6,7,8,9];
+
+let result = num.map((num)=>num*10).map((num)=>num+1).filter((num)=>num>40);
+
+console.log(result);
