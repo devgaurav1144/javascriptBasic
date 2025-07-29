65d8db9199adc5f1aa01ce31b38c02a8a51582b0
Gaurav Singh <devsinghgaurav.1144@gmail.com>
2025-07-29 12:13:19 +0530

    updated reduces

--- reduce.js
+++ reduce.js
@@ -1,8 +1,20 @@
+// reduce() takes all items of the array and reduces them into one single value (like sum, product, etc.)
+
+// It has two parts:
+
+// accum → accumulator (total so far)
+
+// curr → current item in array
+
+// You also gave an initial value of 3
+
+//  initial value must be required because first time accumulator value intialized with initial value.
+
 const numlist = [1,2,3];
 
 const result = numlist.reduce((accum,curr)=>{
     console.log(`Accum Value :${accum} , current Value : ${curr}`);
-return accum+curr
-},3);
+    return accum+curr
+});
 
 console.log(result);
