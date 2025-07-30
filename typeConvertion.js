824222768405363085388a1a6fde974769df0f54
Gaurav Singh <devsinghgaurav.1144@gmail.com>
2025-07-30 16:40:27 +0530

    Added Type Convertion

---
+++ typeConvertion.js
@@ -0,0 +1,12 @@
+ let value = "33"
+let value2 = "33abc"
+let value3 = null
+let value4 = undefined
+let value5 = true
+let isboolean = 1;
+console.log(Number(value)) // 33
+console.log(Number(value2)) //NaN
+console.log(Number(value3)) //0
+console.log(Number(value4))//NaN
+console.log(Number(value5))//1
+console.log(Boolean(isboolean))//true
