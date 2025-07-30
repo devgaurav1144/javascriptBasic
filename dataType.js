f9f030f3878991ebf953d1774fdb659e0130c1bc
Gaurav Singh <devsinghgaurav.1144@gmail.com>
2025-07-30 11:48:41 +0530

    Adding varible and dataType

---
+++ dataType.js
@@ -0,0 +1,18 @@
+"use strict" // treat all js code as newer version
+//alert("Hello") // we are node js not broswer
+
+let num = 8;
+console.log(typeof num)
+
+//number
+// Boolean
+// BigInt
+// String
+// null (standalone value)
+// undefined
+//Symbol => uniqueness
+
+//object 
+
+console.log(typeof null) // object
+console.log(typeof undefined) // undefined
---
+++ variable.js
@@ -0,0 +1,7 @@
+const empname ="Gaurav"
+const mobile=90448657771
+const email = "test@webkul.com"
+
+accountCity = "Lucknow"
+
+console.table([empname,mobile,email,accountCity])
