46c1d2a9eb346f7a9a56762fb0466a73d60939f6
Gaurav Singh <devsinghgaurav.1144@gmail.com>
2025-07-31 17:08:18 +0530

    Adding starttask file

---
+++ starttask.bat
@@ -0,0 +1,33 @@
+@echo off
+echo Starting automation tasks...
+
+:: Core java for Beginners
+start https://www.youtube.com/playlist?list=PLyel0RuhaDoBNVvf5kvPq7P0Z5j9OY6CR
+
+:: Python Tutorial For Beginners in Hindi | Complete Python Course
+start https://www.youtube.com/watch?v=UrsmFxEIp5k&t=129s
+
+:: Spring Boot Full Course for Beginners 2024-25
+start https://www.youtube.com/playlist?list=PLyel0RuhaDoB6hTZDAy5fddmzLhgXqz2F
+
+:: Java Full Stack Course Free For Everyone
+start https://www.youtube.com/playlist?list=PLyel0RuhaDoDT-lYqi3bieLB9Ib5K6drY
+
+:: Docker In One Shot - Part 1 (Piyush Garg)
+start https://www.youtube.com/watch?v=31k6AtW-b3Y&t=3333s
+
+::Docker In One Shot - Part 2(Piyush Garg)
+start https://www.youtube.com/watch?v=xPT8mXa-sJg&t=1101s
+
+
+:: Javascript :: Chai aur Javascript(Hitesh)
+
+start https://www.youtube.com/playlist?list=PLu71SKxNbfoBuX3f4EOACle2y-tRC5Q37
+
+:: Javascript :: CodeWithHarray
+
+start https://www.youtube.com/playlist?list=PLu0W_9lII9ahR1blWXxgSlL4y9iQBnLpR
+
+echo All tasks completed.
+pause
+
