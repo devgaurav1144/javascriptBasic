3729b61b0ac9906e3f6aabc4c2f41509e40291ab
Gaurav Singh <devsinghgaurav.1144@gmail.com>
2025-08-01 11:48:09 +0530

    Datatype summary

---
+++ datatypesummary.js
@@ -0,0 +1,26 @@
+// primaitive 
+
+
+// 7 type: String ,Number,Boolean,null,undefined,Symbol,BigInt
+
+
+// Refference Type
+
+
+// Array,objects,Functions
+
+
+
+
+// const id = Symbol("123");
+// const name = Symbol("123");
+// console.log(name == id)
+// console.log(name)
+
+
+
+const funValue = function(){
+    console.log("Hello World")
+}
+
+console.log(typeof funValue)
