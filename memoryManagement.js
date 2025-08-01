d654ca0af0a84611631a0ea306ba83a097e177c2
Gaurav Singh <devsinghgaurav.1144@gmail.com>
2025-08-01 14:49:18 +0530

    Memory Managament

---
+++ memoryManagement.js
@@ -0,0 +1,16 @@
+// Stack (Primitive)
+
+
+// whenever use stack memory so we get copy of variable not actual value 
+
+
+
+// Heap (Non-Primitive)
+
+// whenever use Heap memory so we get reference value  of variable is actual value 
+
+
+
+// ✅ In JavaScript, primitive types are stored in the stack and are copied when assigned or passed.
+// ✅ Reference types store a reference in the stack to data in the heap, and that reference is copied, not the actual value.
+
